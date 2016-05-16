ec041a9820ec6a6b8cfb6bbc22b7425dc98d72d9
Jacky Boen <aqiank@gmail.com>
2016-05-16 21:26:07 +0800

    Initial commit
    Signed-off-by: Jacky Boen <aqiank@gmail.com>

---
+++ ch3/exit.s
@@ -0,0 +1,8 @@
+.text
+
+    .global _start
+
+_start:
+    movl $1, %eax
+    movl $3, %ebx
+    int $0x80
---
+++ test/hello.s
@@ -0,0 +1,18 @@
+.data
+    str:
+        .ascii "Hello, World!\n"
+
+.text
+
+    .global _start
+
+_start:
+    movl $4, %eax
+    movl $1, %ebx
+    movl $str, %ecx
+    movl $14, %edx
+    int $0x80
+
+    movl $1, %eax
+    movl $0, %ebx
+    int $0x80
